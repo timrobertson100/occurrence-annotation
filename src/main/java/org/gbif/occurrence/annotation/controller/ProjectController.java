f4826e118c03e9a54b24ce014d0aa8f2a42dc811
timrobertson100 <timrobertson100@gmail.com>
2023-03-22 17:33:56 +0100

    Cross origin

--- src/main/java/org/gbif/occurrence/annotation/controller/ProjectController.java
+++ src/main/java/org/gbif/occurrence/annotation/controller/ProjectController.java
@@ -7,6 +7,7 @@ import org.gbif.occurrence.annotation.mapper.ProjectMapper;
 import org.gbif.occurrence.annotation.mapper.RuleMapper;
 import org.gbif.occurrence.annotation.model.Project;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/v1/occurrence/annotation/project")
 public class ProjectController {
   @Autowired private ProjectMapper projectMapper;
--- src/main/java/org/gbif/occurrence/annotation/controller/RuleController.java
+++ src/main/java/org/gbif/occurrence/annotation/controller/RuleController.java
@@ -9,6 +9,7 @@ import org.gbif.occurrence.annotation.mapper.RuleMapper;
 import org.gbif.occurrence.annotation.model.Comment;
 import org.gbif.occurrence.annotation.model.Rule;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/v1/occurrence/annotation/rule")
 public class RuleController {
   @Autowired private RuleMapper ruleMapper;
